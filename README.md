2c2b8923b105297452e37f8502939f4c410fbed6
Haghard <haghard84@gmail.com>
2024-08-04 01:30:02 -0400

    edsl3

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ https://blog.rockthejvm.com/givens-and-implicits/
 https://github.com/zio/zio-constraintless/blob/master/examples/shared/src/main/scala/zio/constraintless/examples/Expr.scala
 
 
+
+https://blog.rockthejvm.com/practical-type-level-programming/
+https://github.com/ncreep/scala3-flat-json-blog
+
+https://github.com/MateuszKubuszok/Scala2vsScala3MacrosExamples/tree/master
+https://www.reddit.com/r/scala/comments/1eaz82v/i_compiled_a_list_of_scala_3_metaprograming/
+https://youtu.be/olBrvPJlXJc
+
 ## License
 
 This code is open source software licensed under the [Apache 2.0 License](http://www.apache.org/licenses/LICENSE-2.0.html).
--- src/main/scala/edsl3/edsl3.scala
+++ src/main/scala/edsl3/edsl3.scala
@@ -5,23 +5,22 @@
 package edsl3
 
 sealed trait Num[T] {
-  def ++(a: T, b: T): T
-  def --(a: T, b: T): T
+  def sum(a: T, b: T): T
+  def minus(a: T, b: T): T
 }
 
 object Num {
   given Integer: Num[Int] with {
     val code: Byte = 0x00
-    def ++(a: Int, b: Int): Int = a + b
-    def --(a: Int, b: Int): Int = a - b
+    def sum(a: Int, b: Int): Int = a + b
+    def minus(a: Int, b: Int): Int = a - b
     override val toString = "i"
   }
 
   given Dbl: Num[Double] with {
     val code: Byte = 0x01
-    def ++(a: Double, b: Double) = a + b
-    def --(a: Double, b: Double) = a - b
-
+    def sum(a: Double, b: Double) = a + b
+    def minus(a: Double, b: Double) = a - b
     override val toString = "d"
   }
 
@@ -30,9 +29,11 @@ object Num {
 // end Num
 
 enum Exp[T] {
-  case IntExp(v: Int) extends Exp[Int]
-  case DblExt(v: Double) extends Exp[Double]
+  case Int32(v: Int) extends Exp[Int]
+  case Int64(v: Long) extends Exp[Long]
+  case Dbl(v: Double) extends Exp[Double]
   case Sum[T](lhs: Exp[T], rhs: Exp[T])(using val ev: Num[T]) extends Exp[T]
+  case Minus[T](lhs: Exp[T], rhs: Exp[T])(using val ev: Num[T]) extends Exp[T]
   case Zip[A, B](lhs: Exp[A], rhs: Exp[B]) extends Exp[(A, B)]
 }
 
@@ -41,14 +42,16 @@ object Exp {
 
   def eval[T](exp: Exp[T]): T =
     exp match {
-      case IntExp(v) =>
+      case Int32(v) =>
         v
-      case DblExt(v) =>
+      case Int64(v) =>
         v
+      case Dbl(v) =>
+        v
+      case s @ Minus(left, right) =>
+        s.ev.minus(eval(left), eval(right))
       case s @ Sum(left, right) =>
-        val l = eval(left)
-        val r = eval(right)
-        s.ev ++ (l, r)
+        s.ev.sum(eval(left), eval(right))
       case zip: Zip[a, b] =>
         (eval(zip.lhs), eval(zip.rhs))
     }
@@ -64,9 +67,13 @@ object Program {
   def apply(): Unit = {
     val exp =
       Zip(
-        Sum(IntExp(1), IntExp(2)),
-        Sum(IntExp(2), IntExp(4)),
+        Zip(
+          Sum(Int32(1), Int32(2)),
+          Sum(Int32(2), Int32(4)),
+        ),
+        Minus(Dbl(21.1), Dbl(4.25)),
       )
+
     println(eval(exp))
   }
 }
